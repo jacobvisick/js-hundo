aeecf82dfc1abc415063d40c5afeb885074b05ff
Jacob Visick <jacobvisick@gmail.com>
2022-07-15 18:06:17 -0400

    Add javascript function

--- js/site.js
+++ js/site.js
@@ -1,8 +1,15 @@
-function getValue() {
-  let msg = document.getElementById("message").value;
-  Swal.fire({
-    backdrop: false,
-    title: "App Name",
-    text: msg,
-  });
+function getNumbers() {
+  let start = parseInt(document.querySelector("#startValue").value);
+  let stop = parseInt(document.querySelector("#stopValue").value);
+  let result = "";
+
+  if (Number.isInteger(start) && Number.isInteger(stop)) {
+    for (let i = start; i <= stop; i++) {
+      result += `${i} `;
+    }
+  } else {
+    result = "Please enter numbers only.";
+  }
+
+  document.querySelector("#results").innerHTML = result;
 }
